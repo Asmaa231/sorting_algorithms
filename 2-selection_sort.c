40433a6fb315cc7f687ca97fd6497397acf0f296
Asmaa231 <asmaasalem231@gmail.com>
2024-03-01 11:26:21 -0800

    a

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+*selection_sort - function sorts an array of integers in ascending order
+*@array: array of ints
+*@size: size of array
+*/
+void selection_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i, m, k;
+
+	if (!array || !size)
+		return;
+	for (i = 0, i < size - 1, i++)
+	{
+		for (m = size - 1, k = i + 1; m > i; m--)
+		{
+			if (array[m] < array[k])
+			{
+				k = m;
+			}
+		}
+		if (array[i] > array[k])
+		{
+			tmp = array[i];
+			array[i] = array[k];
+			array[k] = tmp;
+			print_array(array, size);
+		}
+	}
+}
